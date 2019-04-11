0380cbc677227cfbfa1297c70a62fa998d83ef6b
jobehi <naru.inc@gmail.com>
2019-04-11 15:32:11 +0100

    first commit - Angular material Installation

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,97 @@
+import {
+    MatSelectModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatStepperModule,
+    MatButtonToggleModule,
+    MatRadioModule,
+    MatTabsModule
+  } from '@angular/material';
+
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTreeModule} from '@angular/material/tree';
+import {NgModule} from '@angular/core';
+
+
+@NgModule({
+    imports: [
+      MatSelectModule,
+      MatCheckboxModule,
+      MatBadgeModule,
+      MatDialogModule,
+      MatSlideToggleModule,
+      MatChipsModule,
+      MatTreeModule,
+      MatButtonModule,
+      MatMenuModule,
+      MatIconModule,
+      MatCardModule,
+      MatSliderModule,
+      MatProgressBarModule,
+      MatAutocompleteModule,
+      MatInputModule,
+      MatGridListModule,
+      MatSnackBarModule,
+      MatProgressSpinnerModule,
+      MatTooltipModule,
+      MatListModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatExpansionModule,
+      MatStepperModule,
+      MatButtonToggleModule,
+      MatRadioModule,
+      MatTabsModule
+    ],
+    exports: [
+      MatSelectModule,
+      MatCheckboxModule,
+      MatBadgeModule,
+      MatSlideToggleModule,
+      MatChipsModule,
+      MatTreeModule,
+      MatButtonModule,
+      MatMenuModule,
+      MatIconModule,
+      MatCardModule,
+      MatSliderModule,
+      MatProgressBarModule,
+      MatAutocompleteModule,
+      MatInputModule,
+      MatGridListModule,
+      MatSnackBarModule,
+      MatProgressSpinnerModule,
+      MatTooltipModule,
+      MatListModule,
+      MatDialogModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatExpansionModule,
+      MatStepperModule,
+      MatButtonToggleModule,
+      MatRadioModule,
+      MatTabsModule
+    ],
+  })
+
+  export class MaterialModule {
+  }
+
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {MaterialModule} from './material.module';
+
+
+
+@NgModule({
+  imports: [
+    MaterialModule,
+  ],
+  exports: [
+    MaterialModule,
+  ]
+})
+
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    };
+  }
+}
